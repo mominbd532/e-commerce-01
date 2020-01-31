681b24445aeafc52771aaedf1436b06061fed8de
MD MOMINUL ISLAM <mominbd532@gmail.com>
2020-02-01 03:08:09 +0600

    Show the categories list on index page dinamecaly

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Category;
 use App\Product;
 use Illuminate\Http\Request;
 
@@ -19,6 +20,42 @@ class IndexController extends Controller
 
         $products =Product::inRandomOrder()->get();
 
-        return view('index')->with(compact('products'));
+        //Categories
+
+        $categories =Category::with('categories')->where(['parent_id'=>0])->get();
+        /*$categories1 =json_decode(json_encode($categories));
+        echo "<pre>"; print_r($categories1); die;*/
+
+        /*$categories_manu = "";
+
+        foreach ($categories as $cat){
+
+            $categories_manu .="<div class='panel-heading'>
+                                    <h4 class='panel-title'>
+                                        <a data-toggle='collapse' data-parent='#accordian' href='#".$cat->id."'>
+                                            <span class='badge pull-right'><i class='fa fa-plus'></i></span>
+                                            ".$cat->name."
+                                        </a>
+                                    </h4>
+                                </div>
+                                <div id='".$cat->id."' class='panel-collapse collapse'>
+                                    <div class='panel-body'>
+                                        <ul>";
+                                        $sub_categories =Category::where(['parent_id'=>$cat->id])->get();
+                                         foreach ($sub_categories as $sub_cat){
+
+                                          $categories_manu .= "<li><a href='".$sub_cat->url."'>".$sub_cat->name."</a></li>";
+                                          
+                                                   }
+
+
+            $categories_manu .= "</ul>
+                                    </div>
+                                </div>";
+
+        }*/
+
+
+        return view('index')->with(compact('products','categories'));
     }
 }
