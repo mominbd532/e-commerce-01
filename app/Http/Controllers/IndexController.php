c43ef7ea80ddbe1be41f387aab13cf560bf50ccd
MD MOMINUL ISLAM <mominbd532@gmail.com>
2020-02-01 01:14:09 +0600

    Show product in index page dunamicaly

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -2,11 +2,23 @@
 
 namespace App\Http\Controllers;
 
+use App\Product;
 use Illuminate\Http\Request;
 
 class IndexController extends Controller
 {
     public function index(){
-        return view('index');
+        //Product by ascending
+        $products =Product::get();
+
+        //Product by Descending
+
+        $products =Product::orderBy('id','DESC')->get();
+
+        //Product by Random
+
+        $products =Product::inRandomOrder()->get();
+
+        return view('index')->with(compact('products'));
     }
 }
